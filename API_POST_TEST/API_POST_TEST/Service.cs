c841a405b5571f91dbb43d97197efb57db60b7b9
it.sgusev@gmail.com <it.sgusev@gmail.com>
2025-08-03 01:02:04 +0400

    feat: добавлен тест для Post запроса с авторизацией

---
+++ API_POST_TEST/API_POST_TEST/Service.cs
@@ -0,0 +1,33 @@
+﻿namespace API_POST_TEST;
+
+public static class Service
+{
+    private const string Url = "https://reqres.in/api/";
+    private const string ApiKey = "reqres-free-v1";
+
+    private static readonly HttpClient HttpClient;
+
+    static Service()
+    {
+        HttpClient = new HttpClient();
+        HttpClient.BaseAddress = new Uri(Url);
+        HttpClient.DefaultRequestHeaders.Add("x-api-key", ApiKey);
+    }
+    
+    public static HttpClient GetClient() => HttpClient;
+}
+
+public class UserRequest
+{
+    public string Username {get; set;}
+    public string Email {get; set;}
+    public string Password {get; set;}
+}
+
+public class UserResponse
+{
+    public string Username {get; set;}
+    public string Email {get; set;}
+    public string Password {get; set;}
+    public int Id {get; set;}
+}
---
+++ API_POST_TEST/API_POST_TEST/UserTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace API_POST_TEST;
+
+[TestClass]
+public class UserTests
+{
+    [TestMethod]
+    public async Task User_Should_Be_Register_201_Created()
+    {
+        const string path = "users/register";
+        
+        var user = new UserRequest
+        {
+            Username = "tracey.ramos",
+            Email = "tracey.ramos@reqres.in",
+            Password = "Qwerty123"
+        };
+
+        var jsonRequest = JsonSerializer.Serialize(user, new JsonSerializerOptions { WriteIndented = true });
+        var content = new StringContent(jsonRequest, Encoding.UTF8, @"application/json");
+
+        var client = Service.GetClient(); 
+        var response = await client.PostAsync(path, content);
+        response.EnsureSuccessStatusCode(); // статус ответа 201
+
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        Assert.IsNotEmpty(jsonResponse); // содержимое ответа не пустое
+        
+        var userResponse = JsonSerializer.Deserialize<UserResponse>(jsonResponse);
+        Assert.IsNotNull(userResponse!.Id); // ИД пользователя существует
+        Assert.AreEqual(user.Username, userResponse!.Username);
+        Assert.AreEqual(user.Email, userResponse!.Email);
+        Assert.AreEqual(user.Password, userResponse!.Password);
+    }
+}
